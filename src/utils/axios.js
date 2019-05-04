992d180d5cbfa4af9740c38af9fb59b4fbc416b8
YvesIraguha <iraguhaivos@gmail.com>
2019-05-04 18:33:19 +0200

    fix the environment variables'

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const token = localStorage.getItem("token");
-const baseURL = "http://localhost:5070/api/v1";
+const baseURL = process.env.BASE_URL;
 
 const http = axios.create({
   baseURL,
