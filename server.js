62995b0221e15c9c12e697e307f0a9680d3e9ddf
YvesIraguha <iraguhaivos@gmail.com>
2019-05-04 19:23:13 +0200

    set up server for deploying to heroku

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const path = require("path");
+const port = process.env.port || 8080;
+
+const app = express();
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, "dist")));
+
+app.get("/ping", (req, res) => res.send("pong"));
+
+app.get("/*", (req, res) =>
+  res.sendFile(path.join(__dirname, "dist", "index.html"))
+);
+
+app.listen(port);
