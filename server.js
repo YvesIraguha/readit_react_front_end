9efae033d6a213990eddf902d8d041fbfeb8820b
YvesIraguha <iraguhaivos@gmail.com>
2019-05-04 20:21:07 +0200

    fix deploy error

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
+const dotenv = require("dotenv");
 const path = require("path");
-const port = process.env.port || 8080;
+dotenv.config();
+const port = process.env.PORT || 8080;
 
 const app = express();
 app.use(express.static(__dirname));
